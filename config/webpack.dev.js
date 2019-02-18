e70abadcece18da53785ed9fd8bc9979d93e1faa
sachin8516 <sachin8516@gmail.com>
2019-02-18 13:10:29 +0530

    first commit

---
+++ config/webpack.dev.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: ["./src/js/main.js"]
+    },
+    mode: "development",
+    output: {
+        filename: "js/[name]_bundle.js",
+        path: path.resolve(__dirname, "../dist"),
+        publicPath: "/"
+    },
+    devServer: {
+        contentBase: "dist",
+        overlay: true,
+        hot: true,
+        stats: {
+            colors: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [{loader: "style-loader"},{loader: "css-loader"}]
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader"},
+                    {loader: "postcss-loader"},
+                    {loader: "stylus-loader"}
+                ]
+            },
+            {
+                test: /\.html$/,
+                use:[
+                    {
+                        loader: "html-loader",
+                        options: {
+                            attrs: ["img:src"]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|gif|png)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "images/[name]-[hash:8].[ext]"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+}
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+require("babel-runtime/regenerator");
+require("webpack-hot-middleware/client?reload=true");
+require("bootstrap/dist/css/bootstrap.min.css");
+require("../stylus/main.styl");
+require("jquery");
+require("popper.js");
+require("bootstrap/dist/js/bootstrap.min");
+require("../index.html");
